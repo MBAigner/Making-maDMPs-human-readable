af37c79117ebe7b85f82cc9cc783246287e9acab
Michael Benedikt Aigner <e1529096@student.tuwien.ac.at>
2019-05-30 17:55:47 +0200

    Implemented additional proof of concept @MBAigner @AleksVap

--- scripts/translateMaDMP.py
+++ scripts/translateMaDMP.py
@@ -1,7 +1,9 @@
 import json
+import jinja2
+import pdfkit
 
 input = "../maDMPs/sample.json"
-output = "../hrDMPs/out.pdf"
+output = "../hrDMPs/out"
 template = "FWF"
 
 def get_contact(ma_dmp):
@@ -16,8 +18,26 @@ def parse_contact(ma_dmp):
     return "The contact person of the project is " + name + ". They can be reached on the mail " \
            + mail + ". Their " + type + " is " + id + "."
 
+
+def create_html(text):
+    global template
+    global output
+
+    templateLoader = jinja2.FileSystemLoader(searchpath="../templates/")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    TEMPLATE_FILE = "template_" + template.lower() + ".html"
+    real_template = templateEnv.get_template(TEMPLATE_FILE)
+
+    outputText = real_template.render(contact=text)
+    html_file = open(output + ".html", 'w')
+    html_file.write(outputText)
+    html_file.close()
+
+
 with open(input, 'r') as f:
     ma_dmp = json.load(f)
 
-print(parse_contact(ma_dmp))
+contact = parse_contact(ma_dmp)
+print(contact)
+create_html(contact)
 
