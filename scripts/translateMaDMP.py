5d47cb89da4ff67e5ea7864268d6f57df578957f
Michael Benedikt Aigner <e1529096@student.tuwien.ac.at>
2019-06-30 13:57:53 +0200

    creation of pdf document template changes (now look like the original ones), but adaptions necessary

--- scripts/translateMaDMP.py
+++ scripts/translateMaDMP.py
@@ -1,10 +1,17 @@
 import json
 import jinja2
 import pdfkit
+# import wkhtmltopdf
+import subprocess
 
 input = "../maDMPs/sample.json"
 output = "../hrDMPs/out"
 template = "FWF"
+
+pdf_options = {
+    'page-size': 'A4',
+    'orientation': 'landscape'
+}
 
 def get_contact(ma_dmp):
     return ma_dmp["dmp"]["contact"]["name"], \
@@ -19,25 +26,52 @@ def parse_contact(ma_dmp):
            + mail + ". Their " + type + " is " + id + "."
 
 
-def create_html(text):
-    global template
-    global output
+def create_html(text, template, output):
+    """
+    Creates an HTML file with given template and content.
+    :param text: Demo, just contact information
+    :param template: FWF or Horizon template file
+    :param output: Name of the output file
+    :return: Name of the output file (html)
+    """
 
-    templateLoader = jinja2.FileSystemLoader(searchpath="../templates/")
+    #templateLoader = jinja2.FileSystemLoader(searchpath="../templates/new")
+    templateLoader = jinja2.FileSystemLoader(searchpath="../templates")
     templateEnv = jinja2.Environment(loader=templateLoader)
     TEMPLATE_FILE = "template_" + template.lower() + ".html"
     real_template = templateEnv.get_template(TEMPLATE_FILE)
 
     outputText = real_template.render(contact=text)
-    html_file = open(output + ".html", 'w')
+    html_file = open(output + ".html", "w")
     html_file.write(outputText)
     html_file.close()
+
+    return output + ".html"
+
+
+def create_pdf(html, options):
+    """
+    Converts a given HTML file into a PDF.
+    :param html: HTML file
+    :param options: options for formatting the PDF file
+    :return: name of the resulting pdf file
+    """
+
+    # TODO: we will change this path, or use an other library for converting PDF!
+    path_wkthmltopdf = "C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe"
+    config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
+
+    pdfkit.from_file(html, html.replace(".html", ".pdf"), configuration=config, options=options)
+    return html + ".pdf"
 
 
 with open(input, 'r') as f:
     ma_dmp = json.load(f)
 
+
 contact = parse_contact(ma_dmp)
 print(contact)
-create_html(contact)
-
+html_file = create_html(contact, template, output)
+print(html_file)
+pdf_file = create_pdf(html_file, pdf_options)
+print(pdf_file)
