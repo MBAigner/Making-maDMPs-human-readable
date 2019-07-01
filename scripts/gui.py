e91ce94f7e89baedd835584cbbdb5e036a4f5ef8
Michael Benedikt Aigner <e1529096@student.tuwien.ac.at>
2019-07-02 00:22:12 +0200

    removed redundant data officer processing

--- scripts/gui.py
+++ scripts/gui.py
@@ -198,9 +198,6 @@ def fillInTemplate(event=None):
             with open(file_name, 'r') as f:
                 ma_dmp = json.load(f)
 
-            dataOfficerText.delete("1.0", END)
-            dataOfficerText.insert(END, parse_contact(ma_dmp))
-
             cost_data = parse_cost(ma_dmp)
             datasetData = parse_dataset(ma_dmp)
             dataDescriptionText1.delete("1.0", END)
